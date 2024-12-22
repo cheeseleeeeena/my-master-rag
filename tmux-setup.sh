2c545faa31da4a4abba68e309854c5eb9caf0451
cheeseleeeeena <mie.wenn@icloud.com>
2024-12-22 18:25:23 +0800

    chores: update tmux setup script

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -25,36 +25,40 @@ if [ $? != 0 ]; then
     tmux new-window -t $SESH -n "main"
     tmux send-keys -t $SESH:main "export HF_HOME=$HF_HOME_DIR" C-m
     tmux send-keys -t $SESH:main "cd $PROJECT_DIR" C-m
-    tmux send-keys -t $SESH:main "tree ." C-m
     tmux send-keys -t $SESH:main "conda activate $ENV_NAME" C-m
 
     # create new window for INDEXER program
     tmux new-window -t $SESH -n "indexer"
     tmux send-keys -t $SESH:indexer "export HF_HOME=$HF_HOME_DIR" C-m
     tmux send-keys -t $SESH:indexer "cd $PROJECT_DIR" C-m
-    tmux send-keys -t $SESH:indexer "tree ." C-m
     tmux send-keys -t $SESH:indexer "conda activate $ENV_NAME" C-m
 
     # create new window for PREDICTOR program
     tmux new-window -t $SESH -n "predict"
     tmux send-keys -t $SESH:predict "export HF_HOME=$HF_HOME_DIR" C-m
     tmux send-keys -t $SESH:predict "cd $PROJECT_DIR" C-m
-    tmux send-keys -t $SESH:predict "tree ." C-m
     tmux send-keys -t $SESH:predict "conda activate $ENV_NAME" C-m
     
     # create new window for EVALUATOR program
     tmux new-window -t $SESH -n "eval"
     tmux send-keys -t $SESH:eval "export HF_HOME=$HF_HOME_DIR" C-m
     tmux send-keys -t $SESH:eval "cd $PROJECT_DIR" C-m
-    tmux send-keys -t $SESH:eval "tree ." C-m
     tmux send-keys -t $SESH:eval "conda activate $ENV_NAME" C-m
+
+    # create new window for SCP ops
+    tmux new-window -t $SESH -n "scp"
+    tmux send-keys -t $SESH:scp "export HF_HOME=$HF_HOME_DIR" C-m
+    tmux send-keys -t $SESH:scp "cd /workspace/P76125041/my-master-rag/" C-m
+    # tmux send-keys -t $SESH:scp "tree ." C-m
+    tmux send-keys -t $SESH:scp "conda activate $ENV_NAME" C-m
 
     # create new window for misc ops
     tmux new-window -t $SESH -n "misc"
-    tmux send-keys -t $SESH:eval "export HF_HOME=$HF_HOME_DIR" C-m
+    tmux send-keys -t $SESH:misc "export HF_HOME=$HF_HOME_DIR" C-m
     tmux send-keys -t $SESH:misc "cd $PROJECT_DIR" C-m
-    tmux send-keys -t $SESH:misc "tree ." C-m
-    tmux send-keys -t $SESH:eval "conda activate $ENV_NAME" C-m
+    # tmux send-keys -t $SESH:misc "tree ." C-m
+    tmux send-keys -t $SESH:misc "conda activate $ENV_NAME" C-m
+
 
     # Switch back to the main window
     tmux select-window -t $SESH:main
